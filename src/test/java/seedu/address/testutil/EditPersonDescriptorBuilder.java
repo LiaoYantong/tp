bfaf828aa9b13cd826162b8d87391acd4aaf9a93
Liao Yantong <e1484072@u.nus.edu>
2026-03-09 00:16:16 +0800

    Fix line too long issue

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -2,7 +2,6 @@ package seedu.address.testutil;
 
 import java.util.Set;
 
-
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;

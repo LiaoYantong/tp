0efcd89097e6362da967d3c1fc092154be1ed26f
lyon <limzelyon@gmail.com>
2026-03-26 05:01:44 +0800

    Add FilterTagCommand tests

--- src/test/java/seedu/address/logic/parser/FilterTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterTagCommandParserTest.java
@@ -10,10 +10,35 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FilterTagCommand;
+import seedu.address.model.tag.Tag;
 
 public class FilterTagCommandParserTest {
     private static final String VALID_TAG = "Valid";
 
+    private static final String INVALID_ARGUMENTS = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            FilterTagCommand.MESSAGE_USAGE);
+    private final FilterTagCommandParser parser = new FilterTagCommandParser();
+
+    @Test
+    public void parse_allFields_present() {
+        assertParseSuccess(parser, " " + PREFIX_CATEGORY + " " + PREFIX_TAG + VALID_TAG,
+                new FilterTagCommand("Equipment", new Tag(VALID_TAG)));
+        assertParseSuccess(parser, " " + PREFIX_LOCATION + " " + PREFIX_TAG + VALID_TAG,
+                new FilterTagCommand("Room", new Tag(VALID_TAG)));
+    }
+
+    @Test
+    public void parse_compulsoryMissing_failure() {
+        assertParseFailure(parser, " ", INVALID_ARGUMENTS);
+        assertParseFailure(parser, " " + PREFIX_TAG + " " + VALID_TAG, INVALID_ARGUMENTS);
+    }
+
+    @Test
+    public void parse_duplicateFields_failure() {
+        //Too many prefixes
+        assertParseFailure(parser, " " + PREFIX_CATEGORY + PREFIX_LOCATION + PREFIX_TAG + VALID_TAG,
+                INVALID_ARGUMENTS);
+    }
 
 
 }
